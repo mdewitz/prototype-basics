e597761bbe481d03f2ecc211fca1958c9c4e5252
Mika <mikadewitz@gmail.com>
2015-01-29 10:34:01 -1000

    adding spec tests

--- array_extensions.js
+++ array_extensions.js
@@ -7,27 +7,25 @@ var should = chai.should();
 // check for functions and null and exclude them from the returned 
 // value.
 
-
 Array.prototype.toString = function(){
-  for (var i = 0; i<arguments.length; i++){
-    if(this[i]===null){
-      delete array[i];
-  //http://stackoverflow.com/questions/5767325/remove-specific-element-from-an-array
-      array.toString();
-    }return this.toString();
-  }
+  // for (var i = 0; i<arguments.length-1; i++){
+  // //   if(this[i]===null){
+  //     delete array[i];
+  // //http://stackoverflow.com/questions/5767325/remove-specific-element-from-an-array
+  //     array.join();
+    return this.join();
+  // }
 };
 
 // forEach - This method must take a function as an argument and
 // call that function against each element in the Array.
 
-Array.prototype.forEveryItem = function(){
-  function somefunction(){
-    console.log(this.join(''));
+Array.prototype.forEach = function(argFunc){
+  for (var i = 0; i<this.length; i++){
+    return argFunc(this[i]);
   }
-  return this.apply(somefunction);
-  //http://stackoverflow.com/questions/1793845/convert-an-array-into-a-separate-argument-strings
 };
+//Test-->console.log([1, 2, 3, 4, 5].forEach(console.log));
 
 // EXTRA - Take your array functions that you wrote in ArrayFun 
 // and add them to the Array.prototype.
